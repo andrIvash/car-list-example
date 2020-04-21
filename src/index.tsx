0d8009f52adbb877943cc967a6aade0edf6df0b2
andrivash <andrivash@Andriys-MacBook-Pro.local>
2020-04-21 12:25:56 +0300

    init parcel bandler

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import './assets/scss/index.scss';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <>📦</>
+    )
+  }
+}
+
+render(<App />, document.getElementById('app'));
